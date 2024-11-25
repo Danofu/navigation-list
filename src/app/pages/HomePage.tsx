8690188bb2bf622768165799b939642d3aa6e331
Daniil Borysov <danofu13@gmail.com>
2024-11-26 00:39:52 +0100

    fix(stylelint): resolve Stylelint errors
    - Renamed `rootContainer` to `root-container` in `app/pages/HomePage.module.css` to comply with stylelint rules
    - Updated the corresponding component to use the new class name

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import styles from './HomePage.module.css';
 
-function HomePage(): React.ReactElement | null {
+function HomePage(): React.ReactElement {
   return (
     <main className={styles.root}>
-      <div className={styles.rootContainer} />
+      <div className={styles['root-container']} />
     </main>
   );
 }
